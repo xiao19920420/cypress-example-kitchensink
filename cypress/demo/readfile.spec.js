0d1d68c8f8273a7f1a69805488f2af85214996b7
elephant <297434556@qq.com>
2020-03-08 10:13:03 +0800

    增加用例稳定性

--- cypress/demo/readfile.spec.js
+++ cypress/demo/readfile.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
-describe('数据驱动用例集', function() {
-    it("读取json文件", function() {
-        cy.readFile('cypress/integration/MyTests/newdata.json').its('companyname').should('eq', "let's_begin10.320-1")
-    })
+describe('数据驱动用例集', function () {
+  it('读取json文件', function () {
+    cy.readFile('cypress/integration/MyTests/newdata.json').its('companyname').should('eq', 'let\'s_begin10.320-1')
+  })
 })
