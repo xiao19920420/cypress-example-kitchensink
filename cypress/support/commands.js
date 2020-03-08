9af3f92638d4f33373691407ed1a87520a9431ac
elephant <297434556@qq.com>
2020-03-08 08:55:53 +0800

    完善自动化用例

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable padding-line-between-statements */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -7,8 +8,7 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
+
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
@@ -23,3 +23,36 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', () => {
+  cy.request({
+    url: 'http://ec2-52-220-188-0.ap-southeast-1.compute.amazonaws.com:8080/auth/oauth/token',
+    method: 'POST',
+    form: true,
+    headers: {
+      Authorization: 'Basic Z3dlLWFwcDptYWx0',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: {
+      grant_type: 'password',
+      password: 123456,
+      scope: 'app',
+      username: '297434556@qq.com',
+    },
+  }).then((response) => {
+    const time = new Date().getTime()
+    const tokenStr = {
+      'dataType': 'string',
+      'content': response.body.access_token,
+      'datetime': time,
+    }
+    const tenantStr = {
+      'dataType': 'string',
+      'content': response.body.tenant_id,
+      'datetime': time,
+    }
+    window.localStorage.setItem('malt-token', JSON.stringify(tokenStr))
+
+    window.localStorage.setItem('tenant_id',
+      JSON.stringify(tenantStr))
+  })
+})
